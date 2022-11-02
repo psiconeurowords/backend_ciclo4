257f8cd271b997b546526c4de84386e49e39e2a8
jorge pachon <jpachon@unbosque.edu.co>
2022-11-01 19:00:09 -0500

    commit inicial del proyecto

---
+++ main.py
@@ -0,0 +1,12 @@
+import json
+
+from flask import Flask
+from flask import jsonify
+from flask import request
+from flask_cors import CORS
+from waitress import serve
+
+
+if __name__ == '__main__':
+    print("Hola Jorge")
+
